aa97a5d3915ad69b716a8d125be96ef2a53f800e
Xiaoyi Liu <lxy878@gmail.com>
2020-04-22 13:06:51 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,9 +14,13 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+  get '/goodbye/:name' do
+      "Goodbye, #{params[:name]}"
+  end
+
   get "/multiply/:num1/:num2" do
 
   end
 
-  
+
 end
